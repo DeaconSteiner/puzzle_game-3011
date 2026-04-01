71be55e6917617f56984f0a8b41889881afc705a
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-04-01 17:34:55 -0600

    Work on tile class implementation.

--- src/puzzle/Tile.java
+++ src/puzzle/Tile.java
@@ -1,5 +1,26 @@
 package puzzle;
 
+import java.awt.image.BufferedImage;
+
 class Tile {
-    public static void main(String[] args) {}
+    int id;
+    int correctRow, correctCol;
+    int currentRow, currentCol;
+    BufferedImage piece;
+    boolean isEmpty;
+
+    public  Tile(int id, int correctRow, int correctCol, BufferedImage piece, boolean isEmpty) {
+        this.id = id;
+        this.correctRow = correctRow;
+        this.correctCol = correctCol;
+        this.currentRow = correctRow;
+        this.currentCol = correctCol;
+        this.piece = piece;
+        this.isEmpty = isEmpty;
+    }
+
+    public boolean isCorrect() {
+        return currentRow == correctRow && currentCol == correctCol;
+    }
 }
+
